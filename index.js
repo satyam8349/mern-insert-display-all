0ee525ac4933b95decfc1a523ce0b29a9c5f6368
satyam shukla <satyamshukla8349@gmail.com>
2024-09-25 13:42:12 +0530

    add

--- index.js
+++ index.js
@@ -1,71 +1,12 @@
-
-
-
-
-
-
-
-// const express = require("express");
-// const app= express();
-//  const PORT=8000;
-
-//  app.length("/" ,(req,res)=>{
-//     res.send("<h1>welcome to cybron!!");
-//  })
-//  app.listen(PORT,()=>{
-//     console.log(`listening run:${PORT}`);
-//  })
-
-
-
-
-
-// const express =require("express");
-// const app = express();
-//  const PORT= 5000;
-//  const StuRoute =require("./routes/studentRoute")
-
-
- 
-//  app.get("/" ,(req,res)=>{
-//     res.send("<h1> homepage in screen ")
-
-//  })
-//  app.use("/students",StuRoute)
- 
-//  app.listen(PORT,()=>{
-//     console.log('SERVER RUN:${PORT}')
-//  })
-
-
-
-
-// const express=require("express");
-// const app=express();
-// const PORT=4000;
-// const StuRoute=require("./routes/studentRoute")
-
-// app.get("./",(req,res)=>{
-//    res.send("<h1> home page in screen!!!");
-// })
-
-// app.use("/teacher",StuRoute)
-// app.listen(PORT,()=>{
-//    console.log(`server run:${PORT}`)
-// })
-
-
-// const http=require("http");
-//  http.createServer((req,res)=>{
-//     res.write("<h1>hello friends this is my first mern program  ");
-//     res.end();
-//  }).listen(4000);
- 
-  
-
 const express = require("express");
-const app=express();
-app.get("/",(req,res)=>{
-   res.send("<h1>home page in screen!!!");
-})
-app.listen(5000);
+const app= express();
+const StuRouts=require("./routes/studentRoute");
+const teacherRoute=require("./routes/teacherRoute");
+
+app.use("/students",StuRouts);
+app.use("/teachers",teacherRoute)/
+
+
+ app.listen(8000,()=>{
+   console.log("server is running on port 8000");
+ })
--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -1,41 +1,20 @@
-// const express =require("express");
-// const route= express.Router();
 
-// route.get("/",(req,res)=>{
-//     res.send("<h1>student home page</h1>");
-// })
-
-
-// route.get("/stuinfo",(req,res)=>{
-//     res.send("<h1>student information</h1>");
-// })
-
-// route.get("/stufees",(req,res)=>{
-//     res.send("<h1>student fees</h1>");
-// })
-
-// module.exports=route;
-
-
-// const express=require("express");
-// const route=express.Router();
-
-// route.get("/",(req,res)=>{
-//     res.send("<h1>teacher home page</h1>");
-// })
-
-// route.get("/subject",(req,res)=>{
-//     res.send("<h1>this is react code</h1>");
-// })
-
-// route.get("/salary",(req,res)=>{
-//      res.send("<h1>this is react subject salary</h1>");
-//  })
-
-//  route.get("/department",(req,res)=>{
-//     res.send("<h2>this id react department!!")
-//  })
-//  module.exports=route;
     
 
+ const express=require("express");
+ const route=express.Router();
  
+ route.get("/stuinfo",(req,res)=>{
+    res.send("<h1> this is route first path");
+ })
+
+
+ route.get("/stuifees",(req,res)=>{
+    res.send("<h1> this is route seacond path");
+ })
+
+
+ route.get("/sturesult",(req,res)=>{
+    res.send("<h1> this is route third path");
+ })
+ module.exports=route;
